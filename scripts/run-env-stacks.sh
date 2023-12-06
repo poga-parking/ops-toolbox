3ce1f6974f5394ddaec9759334d5cde86d759be0
aalexren <chernicaar@gmail.com>
2023-12-06 21:32:41 +0300

    fix: github actions

--- scripts/run-env-stacks.sh
+++ scripts/run-env-stacks.sh
@@ -3,8 +3,8 @@
 SOURCE_PATH=~/poga-deploy
 
 echo "Run logging stack..."
-docker stack deploy -c $SOURCE_PATH/stacks/logging.yml --with-registry-auth logging || { echo 'command failed' ; exit 1; }
+docker stack deploy -c $SOURCE_PATH/stacks/logging.yml logging || { echo 'command failed' ; exit 1; }
 echo "Done!"
 echo "Run monitoring stack..."
-docker stack deploy -c $SOURCE_PATH/stacks/monitoring.yml --with-registry-auth monitoring || { echo 'command failed' ; exit 1; }
+docker stack deploy -c $SOURCE_PATH/stacks/monitoring.yml monitoring || { echo 'command failed' ; exit 1; }
 echo "Done!"
--- scripts/run-service-stacks.sh
+++ scripts/run-service-stacks.sh
@@ -3,5 +3,5 @@
 SOURCE_PATH=~/poga-deploy
 
 echo "Run parking-service stack..."
-docker stack deploy -c $SOURCE_PATH/stacks/services/parking-service.yml --with-registry-auth services || { echo 'command failed' ; exit 1; }
+docker stack deploy -c $SOURCE_PATH/stacks/services/parking-service.yml services || { echo 'command failed' ; exit 1; }
 echo "Done!"
