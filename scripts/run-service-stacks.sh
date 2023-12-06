dcff6654a5a2f2cf0d03a95b6d9bf3fd576063bd
aalexren <chernicaar@gmail.com>
2023-12-06 17:25:37 +0300

    fix: github actions

---
+++ scripts/run-service-stacks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+SOURCE_PATH=~/poga-deploy
+
+echo "Run parking-service stack..."
+docker stack deploy -c $SOURCE_PATH/stacks/services/parking-service.yml --with-registry-auth parking || { echo 'command failed' ; exit 1; }
+echo "Done!"
