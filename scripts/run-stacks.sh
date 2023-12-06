fd6fe5c1e82663786ab722f50f51bff1914de220
aalexren <chernicaar@gmail.com>
2023-12-06 13:54:35 +0300

    fix: github actions

--- scripts/run-stacks.sh
+++ scripts/run-stacks.sh
@@ -3,8 +3,8 @@
 SOURCE_PATH=~/poga-deploy
 
 echo "Run logging stack..."
-docker deploy stack -c $SOURCE_PATH/logging.yml --with-registry-auth logging
+docker stack deploy -c $SOURCE_PATH/logging.yml --with-registry-auth logging
 echo "Done!"
 echo "Run monitoring stack..."
-docker deploy stack -c $SOURCE_PATH/monitoring.yml --with-registry-auth monitoring
+docker stack deploy -c $SOURCE_PATH/monitoring.yml --with-registry-auth monitoring
 echo "Done!"
